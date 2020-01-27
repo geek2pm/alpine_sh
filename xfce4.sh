89080be09c92901bfe372d7da8e865dbf5cdbcbd
geek2pm <geek2pm@163.com>
2020-01-27 09:11:04 +0800

    fix

---
+++ xfce4.sh
@@ -0,0 +1,6 @@
+setup-xorg-base xfce4 xfce4-terminal lightdm-gtk-greeter xfce-polkit xfce4-screensaver consolekit2 dbus-x11
+apk add xf86-video-intel
+apk add xf86-video
+apk add xf86-input-mouse xf86-input-keyboard
+apk add xfce4-screenshooter
+apk add sudo
